f88d1477efa91cdfc221538e00aeab3962eeaab4
Jonty Leslie <83710197+JLesDev@users.noreply.github.com>
2024-11-26 09:34:29 +1100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,31 @@ async fn main() -> Result<()> {
     println!("got value from the server; result={:?}", result);
 
     Ok(())
+
+    /*
+
+
+    async fn say_world() {
+
+        println!("world");
+        
+    }
+
+    #[tokio::main]
+    async fn main() {
+
+        // Calling `say_world()` does not execute the body of `say_world()`.
+        let op = say_world();
+
+        // This println! comes first
+        println!("hello");
+
+        // Calling `.await` on `op` starts executing `say_world`.
+        op.await;
+    } 
+
+*/
+
+
+
 }
